0e052d993fde2e781641ba4a990ca948fa2193fa
Aleksandr Gridiaev <alexandergr1993o@gmail.com>
2025-01-12 21:34:44 +0100

    123

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/:sheetId',
+      path: 'sheet/:sheetId',
       name: 'main',
       component: MainPage,
     },
