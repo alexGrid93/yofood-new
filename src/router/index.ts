46014b56c5524ba204bb41788654bd964719e286
Aleksandr Gridiaev <alexandergr1993o@gmail.com>
2025-01-12 20:51:40 +0100

    fix routes (2)

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/:sheetId',
+      path: '/sheet/:sheetId',
       name: 'main',
       component: MainPage,
     },
