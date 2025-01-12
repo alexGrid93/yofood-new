bd1b81c9f2674f4d54a4c5a6083f2914d9c4e66c
Aleksandr Gridiaev <alexandergr1993o@gmail.com>
2025-01-12 21:20:24 +0100

    add statick plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/yofood-new/',
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+    viteStaticCopy({
+      targets: [{ src: 'dist/index.html', dest: '404.html' }],
+    }),
+  ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
