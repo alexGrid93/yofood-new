bea08bf4f009b60d06e0d125ef8d13535f4b294b
Viktor Seniukov <spinnekopser@gmail.com>
2026-02-12 23:20:21 +0100

    added main func

--- src/components/valentine/ValentineActivateScreen.vue
+++ src/components/valentine/ValentineActivateScreen.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { computed, onBeforeUnmount, onMounted, ref } from "vue"
+import { computed, onBeforeUnmount, onMounted, ref } from 'vue'
 import heartA from '../../assets/heart_a.svg'
 import heartB from '../../assets/heart_b.svg'
 import { useStorage } from '@vueuse/core'
+import { valentines } from '@/constants/valentines.ts'
 
 type Particle = {
   id: number
@@ -38,9 +39,9 @@ const SETTINGS = {
   sizeMax: 76,
 
   // скорость всплытия (px per frame * multiplier)
-  speedMin: 1,
-  speedMax: 2,
-  speedMultiplier: 2.8,
+  speedMin: 0.6,
+  speedMax: 1.4,
+  speedMultiplier: 2.2,
 
   // покачивание по X
   swayAmpMin: 6,
@@ -92,7 +93,7 @@ function rand(min: number, max: number) {
 
 function pickImage() {
   const arr = SETTINGS.images
-  return arr[Math.floor(Math.random() * arr.length)] || ""
+  return arr[Math.floor(Math.random() * arr.length)] || ''
 }
 
 const canRun = computed(() => {
@@ -102,7 +103,7 @@ const canRun = computed(() => {
 
 function updateReducedMotion() {
   if (!window.matchMedia) return
-  reducedMotion.value = window.matchMedia("(prefers-reduced-motion: reduce)").matches
+  reducedMotion.value = window.matchMedia('(prefers-reduced-motion: reduce)').matches
 }
 
 function spawn() {
@@ -198,20 +199,20 @@ function particleStyle(p: Particle) {
 
 onMounted(() => {
   updateReducedMotion()
-  const mq = window.matchMedia?.("(prefers-reduced-motion: reduce)")
-  mq?.addEventListener?.("change", updateReducedMotion)
+  const mq = window.matchMedia?.('(prefers-reduced-motion: reduce)')
+  mq?.addEventListener?.('change', updateReducedMotion)
 
   start()
 
   // чтобы на ресайз корректно работало — можно просто продолжать,
   // а новые частицы будут спавниться уже по новой ширине
   const onResize = () => {}
-  window.addEventListener("resize", onResize)
+  window.addEventListener('resize', onResize)
 
   onBeforeUnmount(() => {
     stop()
-    mq?.removeEventListener?.("change", updateReducedMotion)
-    window.removeEventListener("resize", onResize)
+    mq?.removeEventListener?.('change', updateReducedMotion)
+    window.removeEventListener('resize', onResize)
   })
 })
 
@@ -223,7 +224,7 @@ const isActivateValentineScreen = useStorage('valentineActivateScreen', false)
 
 const onClose = () => {
   isActivateValentineScreen.value = false
-};
+}
 
 function onParticleClick(p: Particle, ev: MouseEvent) {
   if (focused.value) return
@@ -249,7 +250,7 @@ function onParticleClick(p: Particle, ev: MouseEvent) {
   }
 
   // 4) убираем частицу из общего слоя (она "переехала" в overlay)
-  particles.value = particles.value.filter(x => x.id !== p.id)
+  particles.value = particles.value.filter((x) => x.id !== p.id)
 
   // 5) запускаем transition
   requestAnimationFrame(() => {
@@ -258,12 +259,12 @@ function onParticleClick(p: Particle, ev: MouseEvent) {
 }
 
 const focusTransform = computed(() => {
-  if (!focused.value) return "translate3d(0px,0px,0) scale(1)"
+  if (!focused.value) return 'translate3d(0px,0px,0) scale(1)'
 
   const f = focused.value
 
   // ✅ целевая ширина почти на весь экран
-  const padX = 24 // отступы по бокам
+  const padX = -50 // отступы по бокам
   const targetMaxW = window.innerWidth - padX * 2
 
   // ✅ по высоте оставим место под кнопку и верх/низ
@@ -289,21 +290,38 @@ const focusTransform = computed(() => {
   return `translate3d(${tx}px, ${ty}px, 0) scale(${s})`
 })
 
+const isShowOpenButton = ref(false)
+
 function onOpenClick() {
-  // тут показываешь калентинку (модалка/компонент/роут)
-  console.log("OPEN VALENTINE")
+  isShowOpenButton.value = false
 }
 
 function onChooseAgain() {
   focusReady.value = false
   focused.value = null
   isSpawningEnabled.value = true
+  isShowOpenButton.value = false
 }
+
+const valentineText = computed<string>(() => {
+  if (!focused.value) {
+    return 'Будь собой! Ты чудо!'
+  }
+
+  if (focused.value?.id > valentines.length) {
+    return valentines[(focused.value.id % valentines.length) - 1]
+  }
+
+  return valentines[focused.value?.id - 1]
+})
 </script>
 
 <template>
   <div ref="rootEl" class="valentine-activate-screen" aria-hidden="true">
     <button @click="onClose" class="valentine-activate-screen__close">Закрыть</button>
+
+    <h3 :class="{'valentine-activate-screen__hint--hide': focused}" class="valentine-activate-screen__hint">Выбери свою валентинку</h3>
+
     <!-- слой частиц -->
     <div class="valentine-layer">
       <img
@@ -328,31 +346,40 @@ function onChooseAgain() {
         height: focused.startH + 'px',
         transform: focusTransform,
       }"
-      @transitionend="() => (focusReady = true)"
+      @transitionend="
+        () => {
+          focusReady = true
+          isShowOpenButton = true
+        }
+      "
     >
       <img class="focus-img" :src="focused.src" alt="" />
-
-      <button
-        v-if="focusReady"
-        class="focus-btn"
-        @click.stop="onOpenClick"
-      >
-        Открыть
-      </button>
-
-      <button
-        v-if="focusReady"
-        class="focus-btn focus-btn--again"
-        @click.stop="onChooseAgain"
-      >
-        Выбрать еще
-      </button>
     </div>
+
+    <button
+      v-if="focusReady && isShowOpenButton"
+      class="focus-btn focus-btn--open"
+      @click.stop="onOpenClick"
+    >
+      Открыть
+    </button>
+
+    <button v-if="focusReady" class="focus-btn focus-btn--again" @click.stop="onChooseAgain">
+      Выбрать еще
+    </button>
+
+    <p v-if="focusReady && focused" class="valentine-text">
+      {{ valentineText }}
+    </p>
   </div>
 </template>
 
 <style scoped>
 .valentine-activate-screen {
+  --v-t: 46%;
+  --v-w: 92%;
+  --v-h: 15%;
+
   position: fixed;
   inset: 0;
   width: 100%;
@@ -360,21 +387,47 @@ function onChooseAgain() {
   overflow: hidden;
   z-index: 1000;
   background-color: hsla(357, 100%, 86%, 0.58);
+  backdrop-filter: blur(6px);
 }
 
 .valentine-activate-screen__close {
   background-color: transparent;
-  border: none;
-  padding: 4px 8px;
+  border: 2px solid hsl(0, 0%, 0%);
+  padding: 8px 12px;
 
   text-decoration: underline;
-  color: hsl(0, 0%, 100%);
-  font-size: 16px;
+  color: hsl(0, 0%, 0%);
+  font-size: 14px;
+  font-weight: 700;
+
+  box-shadow: 0px 0px 20px 4px rgba(0, 0, 0, 0.22);
+
+  border-radius: 4px;
 
   position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
+  top: 2px;
+  right: 2px;
+}
+
+.valentine-activate-screen__hint {
+  position: absolute;
+  top: 70px;
+  width: 100%;
+  text-align: center;
+  margin: 0;
+
+  font-family: 'Great Vibes', serif;
+  font-size: 50px;
+  color: #414042;
+  font-weight: 700;
+
+  opacity: 1;
+
+  transition: opacity 300ms ease-out;
+}
+
+.valentine-activate-screen__hint--hide {
+  opacity: 0;
 }
 
 .valentine-layer {
@@ -399,7 +452,7 @@ function onChooseAgain() {
   position: fixed;
   display: grid;
   place-items: center;
-  transition: transform 520ms cubic-bezier(.2,.9,.2,1);
+  transition: transform 520ms cubic-bezier(0.2, 0.9, 0.2, 1);
   will-change: transform;
   transform-origin: center center; /* ✅ важно для правильного увеличения */
 }
@@ -413,22 +466,61 @@ function onChooseAgain() {
   filter: drop-shadow(0 10px 22px rgba(0, 0, 0, 0.22));
 }
 
+.valentine-text,
 .focus-btn {
   position: absolute;
+  left: 50%;
+}
+
+.valentine-text,
+.focus-btn.focus-btn--open {
+  position: absolute;
+  left: 50%;
+  top: var(--v-t);
+  transform: translate(-50%, -50%);
+  width: var(--v-w);
+  height: var(--v-h);
+  max-width: var(--v-w);
+  max-height: var(--v-h);
+  color: white;
+}
+
+.valentine-text {
+  margin: 0;
+  text-align: center;
+  align-content: center;
+  font-family: sans-serif;
+  font-weight: 700;
+}
+
+.focus-btn {
   padding: 8px 12px;
   border-radius: 6px;
   border: none;
-  background: white;
   font-weight: 700;
   cursor: pointer;
-  box-shadow: 0 10px 24px rgba(0,0,0,0.22);
-  font-size: 8px;
+  min-height: 40px;
+  background-color: transparent;
+}
+
+.focus-btn.focus-btn--open {
+  font-size: 24px;
+  backdrop-filter: blur(10px);
+  z-index: 1050;
 }
 
 .focus-btn.focus-btn--again {
-  bottom: -15px;
-  padding: 2px 4px;
-  font-size: 4px;
-  border-radius: 2px;
+  bottom: 17%;
+  transform: translateX(-50%);
+  padding: 8px 12px;
+  font-size: 16px;
+  border-radius: 4px;
+  box-shadow: 0 10px 24px rgba(0, 0, 0, 0.22);
+}
+</style>
+
+<style>
+body {
+  overflow: hidden;
 }
 </style>
