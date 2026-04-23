1d29e9a1f00bda342daea109362488d86542def9
Viktor Seniukov <spinnekopser@gmail.com>
2026-04-24 01:24:22 +0200

    fix modal

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -345,7 +345,14 @@ const openHB = () => {
     </div>
   </Flex>
 
-  <Modal v-model:open="isOpenHBModal" title="Записка шефу:" :footer="null" centered>
+  <Modal
+    v-model:open="isOpenHBModal"
+    :footer="null"
+    :getContainer="false"
+    title="Записка шефу:"
+    centered
+    wrapClassName="hb-modal"
+  >
     <p>
       С днюхой, шеф! 🎉<br />Сегодня день рождения у человека, который придумал и создал yofood —
       приложение, которым вы пользуетесь каждый день. Без него не было бы ни этого сервиса, ни нас.
@@ -373,6 +380,10 @@ body {
 
 body:has(.hb-banner) {
   margin-top: 90px;
+}
+
+body:has(.hb-modal) {
+  overflow: hidden;
 }
 
 @keyframes spin {
