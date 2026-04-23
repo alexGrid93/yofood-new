141d843a4f0eb20af549bda51c5bb9c841169dbc
Viktor Seniukov <100833471+ViktorSeniukov@users.noreply.github.com>
2026-04-24 01:14:37 +0200

    added hb banner (#8)
    Co-authored-by: Seniukov Viktor <senyukov.v@promail366.com>

--- src/components/TitleContainer.vue
+++ src/components/TitleContainer.vue
@@ -4,7 +4,7 @@ defineEmits(['clickLogo', 'clickTitle'])
 
 <template>
   <div className="titleContainer">
-    <img @click="$emit('clickLogo')" className="maskotImg" src="../assets/mascot.png" />
+    <img @click="$emit('clickLogo')" className="maskotImg" src="../assets/mascot_hb.png" />
     <h1 @click="$emit('clickTitle')">YoFood</h1>
   </div>
 </template>
